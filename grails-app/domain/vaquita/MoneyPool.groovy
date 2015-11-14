49ba8ce03cfa0c37f6180bcf160c40ac2dcddc2f
rafael <borafael@gmail.com>
2015-11-14 19:29:32 -0300

    added name, description, url and type attributes, along with the MoneyPoolType enum and relationships with User

--- grails-app/domain/vaquita/MoneyPool.groovy
+++ grails-app/domain/vaquita/MoneyPool.groovy
@@ -1,7 +1,20 @@
 package vaquita
 
+enum MoneyPoolType {
+}
+
 class MoneyPool {
 
+	String name
+	String description
+	String url
+	MoneyPoolType type
+
+    static hasMany = [participants: User]
+    static belongsTo = User
+
     static constraints = {
+    	name blank: false
+    	type null: false
     }
 }
