786e5b29cef068fb32e2a621cfe88e3b4768e7cc
rafael <borafael@gmail.com>
2015-11-20 02:45:47 -0300

    removed HEAD marker

--- grails-app/domain/vaquita/MoneyPool.groovy
+++ grails-app/domain/vaquita/MoneyPool.groovy
@@ -9,11 +9,8 @@ class MoneyPool {
 	String description
 	String url
 	MoneyPoolType type
-<<<<<<< HEAD
 
     static hasMany = [participations: Participation]
-=======
->>>>>>> c62f00c4850db49d822d7e0966c28f2314d19d36
 
     static constraints = {
     	name blank: false
--- grails-app/domain/vaquita/User.groovy
+++ grails-app/domain/vaquita/User.groovy
@@ -4,11 +4,8 @@ class User {
 
 	String name
 
-<<<<<<< HEAD
 	static hasMany = [participations: Participation]
 
-=======
->>>>>>> c62f00c4850db49d822d7e0966c28f2314d19d36
     static constraints = {
     	name blank: false
     }
