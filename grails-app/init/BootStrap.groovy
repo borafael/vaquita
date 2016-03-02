69a9027052c1bd2fd640f9557ac1c1d833ebc7f6
rafael <borafael@gmail.com>
2016-03-02 19:45:40 -0300

    se modificaron los usuarios de prueba

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -2,9 +2,10 @@ import vaquita.User
 class BootStrap {
 
     def init = { servletContext ->
-      new User (mail: "a", pass: "1234").save()
-      new User (mail: "b", pass: "1234").save()
+        new User (mail: "borafael@gmail.com", pass: "1234").save()
+        new User (mail: "nicolascoco85@gmail.com", pass: "1234").save()
     }
+
     def destroy = {
     }
 }
