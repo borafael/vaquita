41865e7ec07e3f88e0bb8f68d2f3dda1ccaf8640
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2016-03-03 02:15:21 -0300

    se agregan usuarios de prueba al bootstrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -4,6 +4,9 @@ class BootStrap {
     def init = { servletContext ->
         new User (mail: "borafael@gmail.com", pass: "1234").save()
         new User (mail: "nicolascoco85@gmail.com", pass: "1234").save()
+        new User (mail: "cosso@gmail.com", pass: "1234").save()
+        new User (mail: "simon@gmail.com", pass: "1234").save()
+        new User (mail: "pepeluis@gmail.com", pass: "1234").save()
     }
 
     def destroy = {
